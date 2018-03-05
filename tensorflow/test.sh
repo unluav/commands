30a920c79b26d95e9ebae0794ba1d2dd0b3ce83b
Henry Recker <henryjrecker@gmail.com>
2018-03-05 00:50:05 +0000

    Added some test recordings

--- tensorflow/test.sh
+++ tensorflow/test.sh
@@ -1,6 +1,4 @@
-cd speech_commands
-
-python label_wav.py \
+python /home/pi/tensorflow/tensorflow/examples/speech_commands/label_wav.py \
 --graph=trainedGraph.pb \
 --labels=conv_labels.txt \
 --wav=test.wav 
